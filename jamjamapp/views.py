6b867632960be8e0f497bd9578b0b707d6a9a713
lee <cupid0919@gmail.com>
2021-07-23 02:27:45 +0900

    jamjam frontend

---
+++ jamjamapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def layout1(request):
+    return render(request, 'layout1.html')
+
+
+def layout2(request):
+    return render(request, 'layout2.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def main(request):
+    return render(request, 'main.html')
